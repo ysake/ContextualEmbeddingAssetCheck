9818b103cf61d487190661a0cb0226f298c49dbd
Yuuta Sakai <yuuta.sakai@wellsd.jp>
2026-05-03 20:24:16 +0900

    Add ContextualEmbeddingAssetCheck SwiftUI app
    Initial scaffold for the ContextualEmbeddingAssetCheck SwiftUI app. Adds Xcode project and workspace, asset catalogs (AppIcon, AccentColor, Assets contents), ContentView.swift and app entrypoint, and updates .gitignore to ignore .DS_Store. Project files include default build configurations and target setup.

---
+++ ContextualEmbeddingAssetCheck/ContextualEmbeddingAssetCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextualEmbeddingAssetCheckApp.swift
+//  ContextualEmbeddingAssetCheck
+//
+//  Created by 酒井雄太 on 2026/05/03.
+//
+
+import SwiftUI
+
+@main
+struct ContextualEmbeddingAssetCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
